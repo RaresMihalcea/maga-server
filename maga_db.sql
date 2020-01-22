ec561b163fdeedb7d5676b015b9de01c0f5bd0e7
Eduard-Nicola <nicola.mihai.eduard@gmail.com>
2020-01-22 20:07:40 +0200

    Registrarion route added

---
+++ maga_db.sql
@@ -0,0 +1,78 @@
+CREATE DATABASE IF NOT EXISTS maga_db;
+USE maga_db;
+
+CREATE TABLE IF NOT EXISTS user_credentials (
+	email varchar(100) NOT NULL,
+    password_hash varchar(200) NOT NULL
+);
+ALTER TABLE user_credentials ADD PRIMARY KEY (email);
+
+CREATE TABLE IF NOT EXISTS users (
+	id int(10) NOT NULL,
+    name varchar(100) NOT NULL,
+    email varchar(100) NOT NULL,
+    pic blob,
+    extra varchar(200)
+);
+ALTER TABLE users ADD PRIMARY KEY (id);
+
+CREATE TABLE IF NOT EXISTS user_roles (
+    user_id int(10) NOT NULL,
+	role_id int(10) NOT NULL,
+    name varchar(100) NOT NULL
+);
+ALTER TABLE user_roles ADD PRIMARY KEY (role_id);
+ALTER TABLE user_roles ADD FOREIGN KEY (user_id) REFERENCES users(id);
+
+CREATE TABLE IF NOT EXISTS media (
+	id int(10) NOT NULL,
+    link varchar(100) NOT NULL,
+    type varchar(100) NOT NULL
+);
+ALTER TABLE media ADD PRIMARY KEY (id);
+
+CREATE TABLE IF NOT EXISTS content (
+	id int(10) NOT NULL,
+    status ENUM('publicat', 'in asteptare', 'ciorna') NOT NULL,
+    type_id int(10) NOT NULL,
+    title varchar(100) NOT NULL,
+    content varchar(100) NOT NULL,
+    id_media int(10) NOT NULL,
+    id_author int(10) NOT NULL,
+    extra varchar(200)
+);
+ALTER TABLE content ADD PRIMARY KEY (id);
+ALTER TABLE content ADD FOREIGN KEY (id_media) REFERENCES media(id);
+
+CREATE TABLE IF NOT EXISTS edition (
+	id int(10) NOT NULL,
+    name varchar(100) NOT NULL,
+    min_courses int(10) NOT NULL,
+    max_courses int(10) NOT NULL,
+    min_conferences int(10) NOT NULL,
+    max_conferences int(10) NOT NULL
+);
+ALTER TABLE edition ADD PRIMARY KEY (id);
+
+CREATE TABLE IF NOT EXISTS edition_content (
+	id_relation int(10) NOT NULL,
+    id_content int(10) NOT NULL,
+    id_edition int(10) NOT NULL
+);
+ALTER TABLE edition_content ADD PRIMARY KEY (id_relation);
+ALTER TABLE edition_content ADD FOREIGN KEY (id_content) REFERENCES content(id);
+ALTER TABLE edition_content ADD FOREIGN KEY (id_edition) REFERENCES edition(id);
+
+CREATE TABLE IF NOT EXISTS content_type (
+	id int(10) NOT NULL,
+    name varchar(100) NOT NULL
+);
+ALTER TABLE content_type ADD PRIMARY KEY (id);
+
+CREATE TABLE IF NOT EXISTS content_category (
+	id_category int(10) NOT NULL,
+    id_content int(10) NOT NULL,
+    name varchar(100) NOT NULL
+);
+ALTER TABLE content_category ADD PRIMARY KEY (id_category);
+ALTER TABLE content_category ADD FOREIGN KEY (id_content) REFERENCES content(id);
--- server.js
+++ server.js
@@ -3,6 +3,8 @@ var bodyParser = require('body-parser');
 var logger = require('morgan');
 var methodOverride = require('method-override')
 var cors = require('cors');
+var mysql = require('mysql');
+var sha256 = require('js-sha256');
 
 var whitelist = ['http://localhost:4200']
 var corsOptions = {
@@ -15,17 +17,71 @@ var corsOptions = {
     }
 }
 
+// Connect to DB
+var con = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "Atlantykron",
+    database: "maga_db"
+});
+
+con.connect(function(err) {
+    if (err) throw err;
+    console.log("Connected to mysql!");
+});
+
 var app = express();
 app.use(logger('dev'));
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+//app.use(express.bodyParser());
 app.use(methodOverride());
 app.use(cors(corsOptions));
 
 app.listen(process.env.PORT || 8080);
-console.log('listening on port 8080');
+console.log('Listening on port 8080');
 
 app.get('/hello', function (req, res) {
 
     res.send({ message: "Hello Atlantykron" })
 
+});
+
+app.post('/registration', function (req, res) {
+    // Get post params
+    var email = req.body.email,
+        password = req.body.password;
+
+    console.log("Email is ", email);
+    console.log("Password is ", password);
+
+    // Hash the password to store in DB
+    var pass_sha = sha256.create();
+    pass_sha.update(password);
+    pass_sha.hex();    
+
+    con.connect(function(err) {
+        if (err) throw err;
+        // Check if email exists
+        con.query("SELECT email FROM user_credentials", function (err, result, fields) {
+            if (err) throw err;
+            var ok = 1;
+            for (var key in result) {
+                var value = result[key];
+                if (value === email) {
+                    console.log("User already exists!");
+                    ok = 0;
+                    break;
+                }
+            }
+        });
+        // Insert into DB
+        if (ok == 1) {
+            var sql = "INSERT INTO user_credentials (email, password_hash) VALUES (`$email`, `$pass_sha`)";
+            con.query(sql, function (err, result) {
+                if (err) throw err;
+                console.log("1 record inserted");
+            });
+        }
+    });
 });
